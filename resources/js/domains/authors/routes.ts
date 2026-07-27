ddf2b302dabac94c05c119a966b3e1ee98a2128f
dennisrev <65338270+dennisrev@users.noreply.github.com>
2026-07-27 15:56:44 +0200

    Started implementing adding new authors

--- resources/js/domains/authors/routes.ts
+++ resources/js/domains/authors/routes.ts
@@ -1,5 +1,7 @@
 import Overview from './pages/Overview.vue';
+import Create from './pages/Create.vue';
 
 export const authorRoutes = [
     {path: '/authors', component: Overview, name: 'authors.overview'},
+    {path: '/authors/create', component: Create, name: 'authors.create'},
 ];
