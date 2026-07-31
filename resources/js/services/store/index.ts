48b7335115cdd3c2d3f5d6b5e455971e96d8a669
dennisrev <65338270+dennisrev@users.noreply.github.com>
2026-07-31 13:52:31 +0200

    Added store module factory

---
+++ resources/js/services/store/index.ts
@@ -0,0 +1,43 @@
+import { ref, computed } from 'vue';
+import { deleteRequest, getRequest, postRequest, putRequest } from '../http';
+
+export const storeModuleFactory = (moduleName: string) => {
+    const state = ref<Record<number, any>>({});
+
+    const getters = {
+        all: computed(() => state.value)
+    };
+
+    const setters = {
+        setAll: (items: Array<{id: number; [key: string]: any}>) => {
+            for (const item of items) state.value[item.id] = Object.freeze(item);
+        },
+        deleteById: (id: number) => {
+            delete state.value[id];
+        },
+    };
+
+    const actions = {
+        getAll: async () => {
+            const  { data } = await getRequest(moduleName);
+            if (!data) return;
+            setters.setAll(data);
+        },
+        create: async (item: {id: number; [key: string]: any}) => {
+            const { data } = await postRequest(moduleName, item);
+            if (!data) return;
+            setters.setAll(data);
+        },
+        update: async (item: {id: number; [key: string]: any}) => {
+            const { data } = await putRequest(`${moduleName}/${item.id}`, item);
+            if (!data) return;
+            setters.setAll(data);
+        },
+        delete: async (id: number) => {
+            await deleteRequest(`${moduleName}/${id}`);
+            setters.deleteById(id);
+        }
+    };
+
+    return { getters, setters, actions };
+};
